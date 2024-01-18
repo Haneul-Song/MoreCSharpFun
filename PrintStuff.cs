2c61e8b40e0f24c59da12d4df897fb8918d6eb54
Haneul-Song <haneulisstudying@gmail.com>
2024-01-17 17:37:34 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+        private string language;
+
+        // Contructor
+        public PrintStuff ()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp) 
+        {
+            language = temp;
+        }
+    
+        public void PrintName(string n)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + n + "!");
+            }
+
+            if (language == "KR")
+            {
+            System.Console.WriteLine("안녕, " + n + "!");
+            }
+        }
+    }
+}
